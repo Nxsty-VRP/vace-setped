229d535d1ac67c4d28a0b6a5cc651076274bfee7
Nxsty-VRP <69446530+Nxsty-VRP@users.noreply.github.com>
2020-09-03 21:33:56 +0200

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -4,4 +4,4 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 ESX.RegisterCommand('setped', 'admin', function(source)
 	TriggerClientEvent('bc:OpenMenu', source)
-end, true, {help = "Developer Nxsty#3406")
+end, true, {help = "Developer Nxsty#3406"})
